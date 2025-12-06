0c7759d763b9e4d32ee0fa8d0839ba7f275c0958
Zain Ali <99952754+lnzainn@users.noreply.github.com>
2025-12-06 19:52:25 +0530

    Most horribel code ever written, please dont look

--- day2.py
+++ day2.py
@@ -1,7 +1,7 @@
-# with open('input.txt', 'r') as f:
-#     input_range = f.readline()
+with open('AOC25/input.txt', 'r') as f:
+    input_range = f.readline()
 
-input_range = '100-12000'
+# input_range = '12000-200000'
 invalid_range = []
 
 input_range = input_range.split(',')
@@ -11,28 +11,47 @@ for i in range(0, len(input_range)):
     l1 = int(input_range[i].split('-')[0])
     l2 = int(input_range[i].split('-')[1])
     
-    for j in range(l1, l2 + 1):    # For cases like 13 13
+    for j in range(l1, l2 + 1):   
         if len(str(j)) % 2 == 0:
             if str(j)[0:int(len(str(j))/2)] == str(j)[int(len(str(j))/2):]:
                 invalid_range.append(j)
 
-        elif len(str(j)) % 3 == 0:    # For cases like 13 13 13 
-            if str(j)[0:3] == str(j):
-                pass
+            elif len(str(j)) == 6:
+                if str(j)[0:2] == str(j)[4:6] == str(j)[2:4]:
+                    invalid_range.append(j)
 
+            elif len(str(j)) == 8:
+                if str(j)[0:2] == str(j)[6:8] == str(j)[2:4] == str(j)[4:6]:
+                    invalid_range.append(j)
 
-        else:
-            lj = str(j)
-            ll = len(lj)
-            if lj[0:int(ll/2)] == lj[int(ll/2)+1::] and lj[0] == lj[int(ll/2)]:
+            elif len(str(j)) == 10:
+                if str(j)[0:2] == str(j)[2:4] == str(j)[4:6] == str(j)[6:8] == str(j)[8:10]:
+                    invalid_range.append(j)
+
+            else:
+                continue
+            
+
+        elif len(str(j)) == 3:
+            if str(j)[0] == str(j)[1] == str(j)[2]:
                 invalid_range.append(j)
 
+        elif len(str(j)) == 5:
+            if str(j)[0] == str(j)[1] == str(j)[2] == str(j)[3] == str(j)[4]:
+                invalid_range.append(j)
 
+        elif len(str(j)) == 7:
+            if str(j)[0] == str(j)[1] == str(j)[2] == str(j)[3] == str(j)[4] == str(j)[5] == str(j)[6]:
+                invalid_range.append(j)
 
+        elif len(str(j)) == 9:
+            if str(j)[0:3] == str(j)[3:6] == str(j)[6:9]: 
+                invalid_range.append(j)
 
 
             
 print(invalid_range)
+
 sum = 0
 
 for l in invalid_range:
